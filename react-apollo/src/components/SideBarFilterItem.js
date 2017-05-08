951eccf74a910b3629af2aa36f40653b1f5643ce
TMaszko <t.krzyzowski96@gmail.com>
2017-05-08 22:31:03 +0200

    Using one component for one and many options

--- react-apollo/src/components/SideBarFilterItem.js
+++ react-apollo/src/components/SideBarFilterItem.js
@@ -14,16 +14,22 @@ const handleManyOptions = (checkedOptions, currentValueClicked) =>
 		 optionName =>  optionName === currentValueClicked? {
 		 	checked: !checkedOptions[optionName].checked 
 		 }: {checked: checkedOptions[optionName].checked }
+
 const SideBarFilterItem = ({keyOfItem, singleValue, ...props}) => {
 	return (<li key={`li${keyOfItem}`}>
-		<div key={`${keyOfItem}container`} className='col-md-6'>
-        { singleValue? 
-          <OptionCheckBoxes onChangeOptions={handleSingleOption} {...props}/>
-        : 
-        <OptionCheckBoxes onChangeOptions={handleManyOptions} {...props} />
-    	} 
-		</div>
-	</li>)
+				<div key={`${keyOfItem}container`} className='col-md-6'>
+        		{ singleValue
+        		? 
+          		<OptionCheckBoxes 
+          			onChangeOptions={handleSingleOption} 
+          			{...props}/>
+        		: 
+        		<OptionCheckBoxes 
+        			onChangeOptions={handleManyOptions} 
+        			{...props} />
+    			} 
+				</div>
+			</li>)
 }
 
 export default SideBarFilterItem
